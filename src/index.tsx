b6d41cddf07a160dcfd80596144a067d961b3432
Seongeun Yu <yse1027@gmail.com>
2024-02-01 16:45:57 +0900

    recoil, react-hook-form 연습

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { darkTheme } from './theme';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <ThemeProvider theme={darkTheme}>
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from "styled-components";
+
+const darkTheme:DefaultTheme = {
+    bgColor: "#353b48",
+    textColor: "white",
+    accentColor: "#00a8ff",
+    cardBgColor: "transparent",
+}
+
+export {
+    darkTheme,
+};
